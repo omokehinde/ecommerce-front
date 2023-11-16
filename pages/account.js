4efd5e58c7566cdd5b08409ff7c12955a79f8411
omokehinde <engr.omokehinde@gmail.com>
2023-11-16 12:28:51 +0100

    Categories and Account pages

---
+++ pages/account.js
@@ -0,0 +1,14 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+
+
+export default function Account() {
+  return (
+    <>
+        <Header />
+        <Center>
+            <h1>Account</h1>
+        </Center>
+    </>
+  )
+}
---
+++ pages/categories.js
@@ -0,0 +1,14 @@
+import Center from "@/components/Center";
+import Header from "@/components/Header";
+
+
+export default function Categories() {
+  return (
+    <>
+        <Header />
+        <Center>
+            <h1>categories</h1>
+        </Center>
+    </>
+  )
+}
