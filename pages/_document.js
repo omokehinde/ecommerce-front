1706a8e5dba3c0e6f578e59de307e8227288d3a7
omokehinde <engr.omokehinde@gmail.com>
2023-11-24 12:26:05 +0100

    Meta data

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Ecommerce Front</title>
+        <link rel="icon" href="https://img.icons8.com/fluency/48/procurement.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
